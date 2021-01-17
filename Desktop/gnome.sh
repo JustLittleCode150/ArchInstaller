3064b4f579cd05fb6c9696716292581339aecfc5
JustLittleCode150 <77234590+JustLittleCode150@users.noreply.github.com>
2021-01-17 20:27:57 +0700

    Update gnome.sh

--- Desktop/gnome.sh
+++ Desktop/gnome.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-pacstrap -i /mnt base base-devel linux linux-firmware grub efibootmgr sudo nano xorg xorg-xinit gnome gdm curl opera wget git ntfs-3g neofetch gnome-tweaks os-prober gedit linux-lts linux-lts-headers gparted cups chrome-gnome-shell qt5-base qt5-svg qt5-declarative qt5-quickcontrols 
+pacstrap -i /mnt base base-devel linux linux-firmware grub efibootmgr sudo nano xorg xorg-xinit gnome gdm curl opera wget git ntfs-3g neofetch gnome-tweaks os-prober gedit linux-lts linux-lts-headers gparted cups chrome-gnome-shell qt5-base qt5-svg qt5-declarative qt5-quickcontrols gnome-software-packagekit-plugin
